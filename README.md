c702103b62cd6ce7988ad8a02ce7d746f1a78551
Nikolay Vasilchuk <anonym.tsk@gmail.com>
2022-02-01 19:28:07 +0300

    Automations Extension

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# zigbee2mqtt-extensions
-https://www.zigbee2mqtt.io/advanced/more/user_extensions.html
+# Zigbee2MQTT Extensions
+
+### What are extensions?
+
+[Read this article](https://www.zigbee2mqtt.io/advanced/more/user_extensions.html)
+
+### automations-extension.js
+
+**Allows you to set up simple automations directly in z2m**
+
+_Example (add this into your z2m configuration.yaml):_
+
+```yaml
+ automations:
+   my_first_automation:
+     trigger:
+       entity: test_switch
+       action: single
+     action:
+       entity: Test Plug
+       service: toggle
+```
+
+Supported services: `turn_on`, `turn_off`, `toggle`
---
+++ automations-extension.js
@@ -0,0 +1,75 @@
+const settings = require('../../dist/util/settings');
+const logger = require('../../dist/util/logger').default;
+const Extension = require('../../dist/extension/extension').default;
+const stringify = require('json-stable-stringify-without-jsonify');
+
+const ACTIONS = ['toggle', 'turn_on', 'turn_off'];
+
+class AutomationsExtension extends Extension {
+    constructor(zigbee, mqtt, state, publishEntityState, eventBus, enableDisableExtension,
+        restartCallback, addExtension) {
+        super(zigbee, mqtt, state, publishEntityState, eventBus, enableDisableExtension, restartCallback, addExtension);
+
+        this.mqttBaseTopic = settings.get().mqtt.base_topic;
+
+        const automations = settings.get().automations || {};
+        this.automationsBySource = Object.entries(automations).reduce((result, [_, automation]) => {
+            const entity = automation.trigger.entity;
+            let current = result[entity];
+            if (!current) {
+                current = result[entity] = [];
+            }
+            current.push(automation);
+            return result;
+        }, {});
+
+        logger.info('AutomationsExtension loaded');
+        logger.debug(`Registered automations: ${automations}`);
+    }
+
+    findAndRun(entity, action) {
+        logger.debug(`Looking for automations for entity '${entity}' and action '${action}'`);
+        const automations = this.automationsBySource[entity];
+        if (!automations) {
+            return;
+        }
+
+        for (const automation of automations) {
+            if (automation.trigger.action !== action) {
+                continue;
+            }
+            if (!ACTIONS.includes(automation.action.service)) {
+                continue;
+            }
+
+            logger.debug(`Found automation for entity '${entity}' and action '${action}': ${automation}`);
+
+            const destination = this.zigbee.resolveEntity(automation.action.entity);
+            if (!destination) {
+                logger.debug(`Destination not found for entity '${automation.action.entity}'`);
+                continue;
+            }
+
+            let resultState;
+            if (automation.action.service === 'turn_on') {
+                resultState = 'ON';
+            } else if (automation.action.service === 'turn_off') {
+                resultState = 'ON';
+            } else if (automation.action.service === 'toggle') {
+                const state = this.state.get(destination);
+                resultState = state.state === 'ON' ? 'OFF' : 'ON';
+            }
+
+            logger.info(`Run automation for entity '${entity}' and action '${action}': ${automation}`);
+            this.mqtt.onMessage(`${this.mqttBaseTopic}/${destination.name}/set`, stringify({state: resultState}));
+        }
+    }
+
+    async start() {
+        this.eventBus.onPublishEntityState(this, (data) => {
+            this.findAndRun(data.entity.name, data.message.action);
+        });
+    }
+}
+
+module.exports = AutomationsExtension;
